86f4c728dbb1b2b2f88f4b0124b2275c2c93ad36
prlakshm <123609678+prlakshm@users.noreply.github.com>
2023-07-06 15:41:05 -0400

    New colors added

--- sankey.py
+++ sankey.py
@@ -1,7 +1,8 @@
-import plotly
+import chart_studio.plotly as py
 import plotly.graph_objects as go
 import pandas as pd
-  
+import chart_studio.tools as tls
+
 df = pd.read_excel("CASSI in Cary's Bond Park - Rider Survey_800AM_20230606_Analysis Workbook_20230619.xlsx", "Rider Survey Responses (Final)")
 
 label = ["1 – Very safe","2 – Safe","3 – Neither safe nor unsafe (no opinion)","4 – Unsafe", "5 – Very unsafe", "1 – Very safe","2 – Safe","3 – Neither safe nor unsafe (no opinion)","4 – Unsafe", "5 – Very unsafe"]
@@ -15,7 +16,6 @@ after = [int(y[0]) for y in after_list]
 array = [[0] * 5 for _ in range(5)]
 for i in range(0, len(before)):
     array[before[i]-1][after[i]-1]+=1
-print(array)
 flatten_array = sum(array, [])
 
 source = [0,0,0,0,0,1,1,1,1,1,2,2,2,2,2,3,3,3,3,3,4,4,4,4,4]
@@ -23,19 +23,30 @@ target = [5,6,7,8,9,5,6,7,8,9,5,6,7,8,9,5,6,7,8,9,5,6,7,8,9]
 value = flatten_array
 
 
-colors = ["#0073B3","#5F9DC6","#8DB9D8","#BCD6E8","#BCBCBC",
-          "#0073B3","#5F9DC6","#8DB9D8","#BCD6E8","#BCBCBC",
-          "#0073B3","#5F9DC6","#8DB9D8","#BCD6E8","#BCBCBC",
-          "#0073B3","#5F9DC6","#8DB9D8","#BCD6E8","#BCBCBC",
-          "#0073B3","#5F9DC6","#8DB9D8","#BCD6E8","#BCBCBC"]
-link = dict(source = source, target = target, value = value, color=colors)
+colors = ["#2378bd","#6191ce","#929498","#f69171","#f36e4c",
+          "#2378bd","#6191ce","#929498","#f69171","#f36e4c",
+          "#2378bd","#6191ce","#929498","#f69171","#f36e4c",
+          "#2378bd","#6191ce","#929498","#f69171","#f36e4c",
+          "#2378bd","#6191ce","#929498","#f69171","#f36e4c"]
+
+link = dict(source = source, target = target, value = value, color=colors,
+            hovertemplate="Opinion before shuttle ride: %{source.label}<br />Opinion after shuttle ride: %{target.label}<br />Number of respondents: %{value:.0f}<extra></extra>")
 node = dict(label = label, pad=50, thickness=5, color=colors)
 data = go.Sankey(link = link, node=node)
 
 
-print(data)
-
 fig = go.Figure(data)
 fig.update_layout(font=dict(size=20))
-#fig.show()
-plotly.io.write_html(fig,"sankey_diagram2.html", include_plotlyjs=False, full_html=False)
+fig.show()
+fig.to_html("sankey_diagram.html")
+
+'''
+username = 'prlakshm' # your username
+api_key = 'foxsWSdmQh6ABNyUM1b1' # your api key - go to profile > settings > regenerate key
+tls.set_credentials_file(username=username, api_key=api_key)
+
+py.plot(fig, filename = 'sankey_diagram.html', auto_open=True)
+
+
+'''
+
